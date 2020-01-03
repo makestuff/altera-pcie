23f7235f4e2a530a6b2bc93af89c96948c30cf3e
Chris McClelland <prophet3636@gmail.com>
2020-01-03 15:27:18 +0000

    Renamed ip/pcie to ip/pcie-hip

--- apps/demo/tb-sys/diffs/cv.sh
+++ apps/demo/tb-sys/diffs/cv.sh
@@ -2,6 +2,6 @@
 rm -rf cv
 mkdir cv
 cd cv
-cp ../../../../../ip/pcie/cyclonev/pcie_cv.qsys .
+cp ../../../../../ip/pcie-hip/cyclonev/pcie_cv.qsys .
 $ALTERA/quartus/sopc_builder/bin/qsys-generate --family="Cyclone V" --testbench=STANDARD --testbench-simulation=Verilog --allow-mixed-language-testbench-simulation pcie_cv.qsys
 meld pcie_cv/testbench/pcie_cv_tb/simulation/pcie_cv_tb.v ../../pcie_cv_tb.sv &
